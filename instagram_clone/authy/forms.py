04206634b34022570f97a90d62502b6be4bcbfef
Shrey <shreyshukla1010@gmail.com>
2023-05-07 01:39:41 +0530

    completing auth model

--- instagram_clone/authy/forms.py
+++ instagram_clone/authy/forms.py
@@ -3,29 +3,38 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from authy.models import Profile
 
+
 def ForbiddenUsers(value):
 	forbidden_users = ['admin', 'css', 'js', 'authenticate', 'login', 'logout', 'administrator', 'root',
-	'email', 'user', 'join', 'sql', 'static', 'python', 'delete']
+                    'email', 'user', 'join', 'sql', 'static', 'python', 'delete']
 	if value.lower() in forbidden_users:
 		raise ValidationError('Invalid name for user, this is a reserverd word.')
 
+
 def InvalidUser(value):
 	if '@' in value or '+' in value or '-' in value:
-		raise ValidationError('This is an Invalid user, Do not user these chars: @ , - , + ')
+		raise ValidationError(
+			'This is an Invalid user, Do not user these chars: @ , - , + ')
+
 
 def UniqueEmail(value):
 	if User.objects.filter(email__iexact=value).exists():
 		raise ValidationError('User with this email already exists.')
 
+
 def UniqueUser(value):
 	if User.objects.filter(username__iexact=value).exists():
 		raise ValidationError('User with this username already exists.')
 
+
 class SignupForm(forms.ModelForm):
-	username = forms.CharField(widget=forms.TextInput(), max_length=30, required=True,)
-	email = forms.CharField(widget=forms.EmailInput(), max_length=100, required=True,)
+	username = forms.CharField(widget=forms.TextInput(),
+	                           max_length=30, required=True,)
+	email = forms.CharField(widget=forms.EmailInput(),
+	                        max_length=100, required=True,)
 	password = forms.CharField(widget=forms.PasswordInput())
-	confirm_password = forms.CharField(widget=forms.PasswordInput(), required=True, label="Confirm your password.")
+	confirm_password = forms.CharField(
+		widget=forms.PasswordInput(), required=True, label="Confirm your password.")
 
 	class Meta:
 
@@ -45,14 +54,19 @@ class SignupForm(forms.ModelForm):
 		confirm_password = self.cleaned_data.get('confirm_password')
 
 		if password != confirm_password:
-			self._errors['password'] = self.error_class(['Passwords do not match. Try again'])
+			self._errors['password'] = self.error_class(
+				['Passwords do not match. Try again'])
 		return self.cleaned_data
+
 
 class ChangePasswordForm(forms.ModelForm):
 	id = forms.CharField(widget=forms.HiddenInput())
-	old_password = forms.CharField(widget=forms.PasswordInput(), label="Old password", required=True)
-	new_password = forms.CharField(widget=forms.PasswordInput(), label="New password", required=True)
-	confirm_password = forms.CharField(widget=forms.PasswordInput(), label="Confirm new password", required=True)
+	old_password = forms.CharField(widget=forms.PasswordInput(
+		attrs={'class': 'input is-medium'}), label="Old password", required=True)
+	new_password = forms.CharField(widget=forms.PasswordInput(
+		attrs={'class': 'input is-medium'}), label="New password", required=True)
+	confirm_password = forms.CharField(widget=forms.PasswordInput(
+		attrs={'class': 'input is-medium'}), label="Confirm new password", required=True)
 
 	class Meta:
 		model = User
@@ -66,19 +80,26 @@ class ChangePasswordForm(forms.ModelForm):
 		confirm_password = self.cleaned_data.get('confirm_password')
 		user = User.objects.get(pk=id)
 		if not user.check_password(old_password):
-			self._errors['old_password'] =self.error_class(['Old password do not match.'])
+			self._errors['old_password'] = self.error_class(
+				['Old password do not match.'])
 		if new_password != confirm_password:
-			self._errors['new_password'] =self.error_class(['Passwords do not match.'])
+			self._errors['new_password'] = self.error_class(['Passwords do not match.'])
 		return self.cleaned_data
+
 
 class EditProfileForm(forms.ModelForm):
 	picture = forms.ImageField(required=False)
-	first_name = forms.CharField(widget=forms.TextInput(), max_length=50, required=False)
-	last_name = forms.CharField(widget=forms.TextInput(), max_length=50, required=False)
-	location = forms.CharField(widget=forms.TextInput(), max_length=25, required=False)
+	first_name = forms.CharField(
+		widget=forms.TextInput(), max_length=50, required=False)
+	last_name = forms.CharField(
+		widget=forms.TextInput(), max_length=50, required=False)
+	location = forms.CharField(widget=forms.TextInput(),
+	                           max_length=25, required=False)
 	url = forms.URLField(widget=forms.TextInput(), max_length=60, required=False)
-	profile_info = forms.CharField(widget=forms.TextInput(), max_length=260, required=False)
+	profile_info = forms.CharField(
+		widget=forms.TextInput(), max_length=260, required=False)
 
 	class Meta:
 		model = Profile
-		fields = ('picture', 'first_name', 'last_name', 'location', 'url', 'profile_info')
+		fields = ('picture', 'first_name', 'last_name',
+		          'location', 'url', 'profile_info')
