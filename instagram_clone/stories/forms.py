420a656aea358dcb7f8b588ca0529d1a199ebe64
Shrey <shreyshukla1010@gmail.com>
2023-05-11 15:50:20 +0530

    adding stories

---
+++ instagram_clone/stories/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from stories.models import Story
+
+from django.forms import ClearableFileInput
+
+
+class NewStoryForm(forms.ModelForm):
+	content = forms.FileField(widget=forms.ClearableFileInput(
+		attrs={'multiple': True}), required=True)
+	caption = forms.CharField(widget=forms.Textarea(
+		attrs={'class': 'input is-medium'}), required=True)
+
+	class Meta:
+		model = Story
+		fields = ('content', 'caption')
